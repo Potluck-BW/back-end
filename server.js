6b2d93352f1963fb90e814a7431a277f37150105
Jason Rostar <jason.rostar@yahoo.com>
2020-11-25 10:20:58 -0800

    added server message

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-
 const usersRouter = require('./user/users-router');
 const eventRouter = require('./event_planner/event-router');
-
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-
-
 server.use(eventRouter);
 server.use(usersRouter);
+
+server.get('/', (req, res) => {
+	res.send(`<h2>API UP!</h2>`);
+  });
 
 server.use((err, req, res, next) => {
 	console.log(err)	
